338457d43fe7b8eae68a64d54735014735876047
Sergio Salinas Velez <Sergio.Salinas@jalasoft.com>
2022-03-07 08:25:04 -0400

    add strategy pattern for assertions

---
+++ cypress/integration/strategy/verifyUserData.js
@@ -0,0 +1,65 @@
+import UserInfo from "../pageObject/userInformation";
+
+class NewUserfirstName {
+  verifyData(data) {
+    UserInfo.newUserName().should("have.text", data);
+  }
+}
+
+class NewUserLastName {
+  verifyData(data) {
+    UserInfo.newUserLastname().should("have.text", data);
+  }
+}
+
+class NewUserAge {
+  verifyData(data) {
+    UserInfo.newUserAge().should("have.text", data);
+  }
+}
+
+class NewUserEmail {
+  verifyData(data) {
+    UserInfo.newUserEmail().should("have.text", data);
+  }
+}
+
+class NewUserDepartment {
+  verifyData(data) {
+    UserInfo.newUserDepartment().should("have.text", data);
+  }
+}
+
+class NewUserSalary {
+  verifyData(data) {
+    UserInfo.newUserSalary().should("have.text", data);
+  }
+}
+
+export class UserVerifyTextStrategyManager {
+  constructor() {
+    this.userData = null;
+  }
+  setStrategy(userData) {
+    this.userData = userData;
+  }
+  verifyData(data) {
+    this.userData.verifyData(data);
+  }
+}
+
+const firstName = new NewUserfirstName();
+const lastName = new NewUserLastName();
+const age = new NewUserAge();
+const email = new NewUserEmail();
+const department = new NewUserDepartment();
+const salary = new NewUserSalary();
+
+export const userDataClassLookup = {
+  firstName,
+  lastName,
+  email,
+  age,
+  salary,
+  department,
+};
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,12 +2,23 @@ import {
   RegistrationFormStrategyManager,
   fieldClassLookup,
 } from "../integration/strategy/registrationForm";
+import {
+  UserVerifyTextStrategyManager,
+  userDataClassLookup,
+} from "../integration/strategy/verifyUserData";
 
 Cypress.Commands.add("fillRegistrationForm", (tableObjectValues) => {
   const registrationFormStrategy = new RegistrationFormStrategyManager();
   for (const key in tableObjectValues) {
-    cy.log(key);
     registrationFormStrategy.setStrategy(fieldClassLookup[key]);
     registrationFormStrategy.fillValue(tableObjectValues[key]);
+  }
+});
+
+Cypress.Commands.add("verifyContent", (objectValues) => {
+  const verifyDataStrategy = new UserVerifyTextStrategyManager();
+  for (const key in objectValues) {
+    verifyDataStrategy.setStrategy(userDataClassLookup[key]);
+    verifyDataStrategy.verifyData(objectValues[key]);
   }
 });
